5e9bc031aec71af743cf348413af50774f06fab0
Archit Gupta <rarchitgupta@gmail.com>
2023-11-10 23:20:17 +0530

    Arrays and Slices

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -1,9 +1,18 @@
 package arraysslices
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
 }
--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -1,10 +1,13 @@
 package arraysslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := [5]int{1,2,3,4,5}
+		numbers := []int{1,2,3,4,5}
 		
 		got := Sum(numbers)
 		want := 15
@@ -13,4 +16,25 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d but want %d, given %v", got, want, numbers)
 		}
 	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1,2,3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d but want %d, given %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
